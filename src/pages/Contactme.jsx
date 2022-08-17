4f865fc63dcd09754dd9c5ea2220e5dd943449aa
Alisher Meldebek <devka4an@gmail.com>
2022-08-17 11:37:39 +0600

    #contactme-fix: fix: removed message send

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -2,75 +2,9 @@ import React from 'react'
 import icon from './../static/send-icon.svg'
 
 export default function Contactme({handleNotif}) {
-
-    const sendme = (e) => {
-        e.preventDefault()
-        const data = {
-            sender: e.target[0].value,
-            message: e.target[1].value
-        }
-
-        fetch('https://sendmemail-ka4an.herokuapp.com/sendmemail', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(res => res.json())
-        .then(data => {
-            handleNotif({
-                id: new Date().getTime(),
-                type: 'success',
-                value: `Mail sent!`
-            })
-        }).catch(e => {
-            handleNotif({
-                id: new Date().getTime(),
-                type: 'error',
-                value: `Error on sending mail`
-            })
-        })
-
-        fetch('https://sendmemail-ka4an.herokuapp.com/telegram', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(res => res.json())
-        .then(data => {
-            handleNotif({
-                id: new Date().getTime(),
-                type: 'success',
-                value: `Message sent to my telegram!`
-            })
-        }).catch(e => {
-            handleNotif({
-                id: new Date().getTime(),
-                type: 'error',
-                value: `Error on sending telegram message`
-            })
-        })
-
-        console.log('e', e);
-    }
     return (
         <>
-            <h1>CONTACT ME</h1>
             <div className="container">
-                <form className="form half" onSubmit={sendme}>
-                    <div className="field">
-                        <label htmlFor="sender">your name?</label>
-                        <textarea name="sender" id="sender" cols="30" rows="5"></textarea>
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">your message</label>
-                        <textarea name="message" id="message" cols="30" rows="10"></textarea>
-                    </div>
-                    <button className="btn">SEND <img src={icon} alt="" /></button>
-                </form>
                 <div className="social half">
                     <p>MY SOCIALS:</p>
                     <ul className="social_list">
