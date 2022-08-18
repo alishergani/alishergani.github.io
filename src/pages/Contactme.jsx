8a15fc1c0f500bf1e3ecb6f9c11d61c0b4e24ea3
Alisher Meldebek <alisher.meldebek@kaspi.kz>
2022-08-18 16:56:13 +0600

    #contact-fix fix: mini changes

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -5,20 +5,20 @@ export default function Contactme({handleNotif}) {
     return (
         <>
             <div className="container">
-                <div className="social half">
+                <div className="row">
                     <p>MY SOCIALS:</p>
-                    <ul className="social_list">
-                        <li className="social_item">
+                    <ul className="row_list">
+                        <li className="row_item">
                             <a href="https://t.me/ka4anbek" target="_blank" rel="noreferrer">
                                 <img src="https://web.telegram.org/k/assets/img/safari-pinned-tab.svg?v=jw3mK7G9Ry" alt="telegram" />
                             </a>
                         </li>
-                        <li className="social_item">
+                        <li className="row_item">
                             <a href="https://github.com/alishergani" target="_blank" rel="noreferrer">
                                 <img src="https://cdn-icons-png.flaticon.com/512/25/25657.png" alt="github" />
                             </a>
                         </li>
-                        <li className="social_item">
+                        <li className="row_item">
                             <a href="https://www.instagram.com/ka4anbek/" target="_blank" rel="noreferrer">
                                 <img src="https://cdn-icons-png.flaticon.com/512/1419/1419647.png" alt="instagram" />
                             </a>
