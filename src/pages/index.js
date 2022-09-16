0502f3a8f069bd263633c2510ad31d3f0ed60919
Alisher Meldebek <alisher.meldebek@kaspi.kz>
2022-09-16 16:45:16 +0600

    feature: removed wedding page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export { default as Home } from './Home';
 export { default as About } from './About';
 export { default as Contactme } from './Contactme';
-export { default as Wedding } from './Wedding';
