f555a795fd886f31a95b6fcefde4dcf670f1cb6f
Alisher Meldebek <ameldebek@jmart.kz>
2025-09-09 03:17:33 +0500

    mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,29 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        placeholder for alisher meldebek portfolio
-      </main>
+      <div style={{ marginBottom: "22px" }}>
+        <div style={{ display: "flex" }}>
+          <p style={{ fontWeight: "700", fontSize: "20px" }}>Hi, I'm</p>
+          <p className={styles.name}>ALISHER</p>
+        </div>
+        <p className={styles.name}>MELDEBEK</p>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "14px 0",
+        }}
+      >
+        <h3
+          style={{ fontSize: "44px", lineHeight: "33px", textAlign: "center" }}
+        >
+          FRONTEND DEVELOPER
+        </h3>
+        <h4>SENIOR/MIDDLE/JUNIOR</h4>
+      </div>
     </div>
   );
 }
